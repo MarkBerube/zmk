687d49c2b9fd86c5eb2dc034dc7d9008eb498137
Mark Berube <10816162+MarkBerube@users.noreply.github.com>
2023-12-13 22:48:15 -0600

    Update art.c - not yet.

--- app/boards/shields/nice_view/widgets/art.c
+++ app/boards/shields/nice_view/widgets/art.c
@@ -232,7 +232,8 @@ const lv_img_dsc_t mountain = {
 #define LV_ATTRIBUTE_IMG_NEWLOGO
 #endif
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_NEWLOGO uint8_t newlogo_map[] = {
+const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_NEWLOGO uint8_t
+    newlogo_map[] = {
 #if CONFIG_NICE_VIEW_WIDGET_INVERTED
         0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
         0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
